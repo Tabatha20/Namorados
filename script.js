341ed9dec6e2071e819ead7c842324535d0ada19
Tabatha De Oliveira Arruda Jacobino <118621295+Tabatha20@users.noreply.github.com>
2024-05-19 01:39:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-function showMessage() {
-    var message = document.getElementById('surprise-message');
-    if (message.style.display === 'none') {
-        message.style.display = 'block';
-    } else {
-        message.style.display = 'none';
-    }
+{
+  "name": "namorados",
+  "version": "1.0.0",
+  "description": "Site do Dia dos Namorados",
+  "main": "index.html",
+  "scripts": {
+    "build": "webpack --mode production",
+    "start": "webpack serve --open"
+  },
+  "dependencies": {},
+  "devDependencies": {
+    "webpack": "^5.65.0",
+    "webpack-cli": "^4.9.1",
+    "webpack-dev-server": "^4.7.1",
+    "css-loader": "^6.5.1",
+    "style-loader": "^3.3.1"
+  }
 }
+
+
